947da8d14c4395f6c277e21bba96ec72e115e2e4
alexey <sad_alexxxei@mail.ru>
2022-05-14 04:39:47 +0300

    Изменил метод для получения запроса

--- src/Service/Http/HttpRequest.php
+++ src/Service/Http/HttpRequest.php
@@ -3,33 +3,17 @@ declare(strict_types=1);
 
 namespace App\Service\Http;
 
-use RuntimeException;
 use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
-use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+use Symfony\Contracts\HttpClient\ResponseInterface;
 
 class HttpRequest
 {
-    private const SUCCESS_STATUS_CODE = 200;
-
     /**
      * @throws TransportExceptionInterface
-     * @throws ServerExceptionInterface
-     * @throws RedirectionExceptionInterface
-     * @throws ClientExceptionInterface
      */
-    public static function getHtml(string $url): string
+    public static function getRequest(string $url): ResponseInterface
     {
-        $client = HttpClient::create();
-        $response = $client->request('GET', $url);
-
-        if ($response->getStatusCode() !== self::SUCCESS_STATUS_CODE)
-        {
-            throw new RuntimeException('Invalid server response. Status code: ' . $response->getStatusCode());
-        }
-
-        return $response->getContent();
+        return HttpClient::create()->request('GET', $url);
     }
 }
